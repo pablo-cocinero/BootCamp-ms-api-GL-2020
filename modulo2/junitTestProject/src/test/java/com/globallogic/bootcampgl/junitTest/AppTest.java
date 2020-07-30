1aa9c8be681d2c4ccfa5ef8299563462aaf90b09
Pablo <pablodanielcocinero@gmail.com>
2020-07-30 12:03:00 -0300

    comenzando testing

---
+++ modulo2/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
@@ -0,0 +1,14 @@
+package com.globallogic.bootcampgl.junitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		assertEquals("Test","Test");
+	}
+
+}
